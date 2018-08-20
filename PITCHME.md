274969d1c4f83e9d02565064bb7a71c0ea08fd9b
Fabiano Amaral <fabiano.af@outlook.com>
2018-08-20 13:32:46 -0300

    adiciona a tabela de conversao

--- PITCHME.md
+++ PITCHME.md
@@ -1,17 +1,30 @@
+<!-- $theme: gaia -->
+
 # CLIs + NodeJS
-#### Criando aplicaçoes CLIs com NodeJS de uma forma bonita e rápida
+## Criando aplicaçoes CLIs com NodeJS de uma forma bonita e rápida
+
 Fabiano Amaral Freitas
 
 Computer Engineer at Pagar.me
 
 <!-- ![linkedIn](./icons/linkedin.svg) -->
-<img src="icons/linkedin.svg" style="width: 30px"/> Fabiano-amaral
+<img src="icons/linkedin.svg" style="width: 20px"/> Fabiano Amaral
+
 ---
 
+
 ### Agenda
-- O que São CLIs
+- O que são CLIs
 - Por que são uteis
 - Libs utilizadas
 - Mãos na massa
 
 ---
+
+# O que são CLIs
+
+```js
+const fn = () => {
+  console.log('olá mundo')
+}
+```
--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 #!/usr/bin/env node
+const moment = require('moment')
+const Configstore = require('configstore')
 const clear = require('clear')
 const inquirer = require('./lib/inquirer')
-const { headerBar } = require('./lib/menus')
+const pkg = require('./package.json')
+const {
+  headerBar,
+  buildTable,
+	buildConversionTable
+} = require('./lib/menus')
+const {
+  fetchCurrencys
+} = require('./lib/utils')
+
+const conf = new Configstore(pkg.name)
 
 const run = async () => {
   clear()
   headerBar()
   const credentials = await inquirer.askCredentials()
+
   while(true) {
     clear()
     headerBar()
     const { option } = await inquirer.selectFunction()
+    const now = moment()
+    const leastRequest = moment(conf.get('leastRequest'))
+    let data
+    if(now.isAfter(leastRequest.add(10, 'm'))) {
+      console.log('olá');
+      data = await fetchCurrencys()
+      conf.set('leastRequest', now)
+      conf.set('data', data)
+    }
+    else {
+      data = conf.get('data')
+    }
     switch(option) {
       case 1: // busca api
+        buildTable(data)
+        await inquirer.continuar()
         break
       case 2: // converte
+        const { value }= await inquirer.selectConvert(data)
+				buildConversionTable(data, value)
+				await inquirer.continuar()
         break
       case 3: // sair
         return
